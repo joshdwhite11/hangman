9ca4846429b83208b804b033ef59319829fb62eb
joshdwhite11 <51339125+joshdwhite11@users.noreply.github.com>
2019-06-05 11:21:25 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -26,29 +26,34 @@ while tries >= 1 and word != "".join(guess):
     print("Your guess is: ", "".join(guess))
     letter = input("Guess a letter: ")
     print("")
-    if letter in usedtotal:
-        print("You have already used that letter.")
+    if len(letter) > 1:
+        print("Please just use one letter.")
+        print("")
     else:
-        usedtotal = usedtotal + letter
-        rightcounter = 0
-        lencounter = 0 #Counter to parse word
-        while len(word) > lencounter:
-            if word[lencounter] == letter:
-                guess[lencounter] = letter
-                rightcounter += 1
-                lencounter += 1
-                usedcorrect = usedcorrect + letter
-            else:
-                lencounter += 1
-        if rightcounter == 0:
-            tries = tries - 1
-            print("***********************************************************")
-            print("That is not a correct letter, sorry. You have", tries, "tries left")
-            print("You have used the letters:", usedtotal)
+        if letter in usedtotal:
+            print("You have already used that letter.")
+            print("")
         else:
-            print("***********************************************************")
-            print("Correct guess! Nice.")
-            print("You have used the letters", usedtotal)
+            usedtotal = usedtotal + letter
+            rightcounter = 0
+            lencounter = 0 #Counter to parse word
+            while len(word) > lencounter:
+                if word[lencounter] == letter:
+                    guess[lencounter] = letter
+                    rightcounter += 1
+                    lencounter += 1
+                    usedcorrect = usedcorrect + letter
+                else:
+                    lencounter += 1
+            if rightcounter == 0:
+                tries = tries - 1
+                print("***********************************************************")
+                print("That is not a correct letter, sorry. You have", tries, "tries left")
+                print("You have used the letters:", usedtotal)
+            else:
+                print("***********************************************************")
+                print("Correct guess! Nice.")
+                print("You have used the letters", usedtotal)
 
 if tries == 0:
     print("")
